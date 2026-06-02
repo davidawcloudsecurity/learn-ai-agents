b6b323210bb5ada5a44637e0966a5166573311af
David Aw <89636227+davidawcloudsecurity@users.noreply.github.com>
2026-06-02 21:58:58 +0800

    Modify ALB security group ingress rules
    Updated security group rules to switch ports for HTTP and HTTPS traffic.

--- infra_terraform/main.tf
+++ infra_terraform/main.tf
@@ -362,17 +362,17 @@ resource "aws_security_group" "alb_sg" {
   vpc_id      = aws_vpc.demo_main_vpc[0].id
 
   ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
     from_port   = 443
     to_port     = 443
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["172.16.0.0/16"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["172.16.0.0/16"]
   }
 
   egress {
